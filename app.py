b5ce39db111f615807638d84373fcb30c8d06b7e
Emma Carballal <carballal.emma@gmail.com>
2022-06-01 16:16:32 +0200

    New streamlit page

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ import streamlit as st
 with st.form(key='my_form1'):
 	text_input = st.text_input(label='Describe what you would  like to hear')
 	submit_button = st.form_submit_button(label='Submit')
+
+st.markdown('''
+You want to listen to something like:
+''')
+text_input
