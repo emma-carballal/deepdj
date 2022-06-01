91c8a9654394a12fd8a25d04870c390f4bed7e77
Julia Strahl <j.strahl@gmx.de>
2022-06-01 18:13:38 +0200

    finished cleaning step

--- deepdj/preprocessing.py
+++ deepdj/preprocessing.py
@@ -6,6 +6,7 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 import unidecode
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from scipy.spatial import distance
 
 
 def get_data():
@@ -14,11 +15,17 @@ def get_data():
     df = pd.read_csv('data/tcc_ceds_music_cleaned.csv')
     return df
 
+def prompt():
+    """input the prompt"""
+    value = input('Please enter a string:\n')
+    df = pd.DataFrame({'prompt':value}, index = pd.RangeIndex(0,1))
+    return df
+
 
 
 def cleaning(lyric):
 # Basic cleaning
-    sentence = sentence.strip() ## remove whitespaces
+    sentence = lyric.strip() ## remove whitespaces
     sentence = sentence.lower() ## lowercasing
     sentence = ''.join(char for char in sentence if not char.isdigit()) ## removing numbers
     sentence = unidecode.unidecode(sentence) # remove accents
@@ -40,18 +47,36 @@ def cleaning(lyric):
 
 
 
-def vectorizing(cleaned_sentence):
+def vectorizing(lyric):
     vectorizer = TfidfVectorizer(max_df = 0.5, max_features = 5000, ngram_range=(1,2))
-    vectorized_lyrics = pd.DataFrame(vectorizer.fit_transform(cleaned_sentence).toarray(),
-                                columns = vectorizer.get_feature_names_out())
-    return vectorized_lyrics
+    if df.shape[0] > 1:
 
-if '__name__' == '__main__':
-    df = read_csv()
-    df['lyrics_cleaned'] = df['lyrics'].apply(cleaning)
-    df_input = input()
-    vectorize_input(df_input)
-    print(find_cosine_distance())
+        vectorized_lyrics = pd.DataFrame(vectorizer.fit_transform(df['cleaned']).toarray(),
+                                columns = vectorizer.get_feature_names_out())
+        return vectorized_lyrics
+    else:
+        vectorized_value = pd.DataFrame(vectorizer.transform(df['cleaned']).toarray(),
+                                columns = vectorizer.get_feature_names_out())
+        return vectorized_value
+
+def cos_distance(vectorized_lyrics, vectorized_value):
+    df["distance"] = [distance.cosine(vectorized_lyrics.iloc[k], vectorized_value) for k in range(len(vectorized_lyrics))]
+    closest = df["distance"].nsmallest(60)
+    return closest
+
+
+
+if __name__ == '__main__':
+    #get, clean and vectorize data
+    df = get_data()
+    df_prompt = prompt()
+    df['cleaned'] = df['lyrics'].apply(cleaning)
+    df_prompt['cleaned'] = df_prompt['prompt'].apply(cleaning)
+    vectorized_lyrics = df['cleaned'].apply(vectorizing)
+    vectorized_value = df_prompt['cleaned'].apply(vectorizing)
+    print (cos_distance(vectorized_lyrics, vectorized_value))
+
+
 
 #process new data, text prompt
 #in case of text promt: accept string and return a vector
