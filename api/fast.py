442fc407592fc0d15bcd0a441cb606665743c210
Julia Strahl <j.strahl@gmx.de>
2022-06-02 16:41:09 +0200

    API

--- api/fast.py
+++ api/fast.py
@@ -2,6 +2,7 @@ from importlib import reload
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from deepdj.processing import deepdj_processing
 
 app = FastAPI()
 
@@ -18,8 +19,12 @@ def index():
     return {"greeting": "Welcome to DeepDJ!"}
 
 @app.get("/playlist")
-def playlist():
-    return {"greeting": "Playlist"}
+def playlist(text_input):
+    dj1 = deepdj_processing('data/tcc_ceds_music_cleaned.csv')
+    #return {"greeting": "Playlist"}
+    return deepdj_processing.prompt_process(text_input)
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
