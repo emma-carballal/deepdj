134551bafd4752955b016188c97c88fcc8cf3d6f
Julia Strahl <j.strahl@gmx.de>
2022-06-07 17:48:00 +0200

    changed path to df

--- app.py
+++ app.py
@@ -14,15 +14,15 @@ with st.form(key='my_form1'):
 	submit_button = st.form_submit_button(label='Submit')
 
 
-deepdj_api_url = "https://deepdj-7ah34aow4a-ew.a.run.app"
-# deepdj_api_url = "http://127.0.0.1:8000"
+#deepdj_api_url = "https://deepdj-7ah34aow4a-ew.a.run.app"
+deepdj_api_url = "http://127.0.0.1:8000"
 
 params = {"text_input" : text_input}
 response = requests.get(deepdj_api_url, params=params).json()
 if response['res']!=0:
     final_result = (pd.DataFrame.from_dict(response['res'], orient = 'index'))#turn JSON into DataFrame
 
-    df = pd.read_csv("/Users/justrahl/code/deepdj/api/tcc_ceds_music_cleaned.csv", index_col=False)
+    df = pd.read_csv("tcc_ceds_music_cleaned.csv", index_col=False)
 
     indexes = final_result.index
 
