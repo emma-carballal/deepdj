2af81fb480b1b7e42fa0e4feaf02f35cf13cc973
hp <hatice.peucker@hotmail.com>
2022-06-08 16:07:04 +0200

    heroku test

--- app.py
+++ app.py
@@ -48,11 +48,6 @@ You want to listen to something like:
 ''')
 text_input
 
-with st.form(key='columns_in_form'):
-    cols = st.columns(5)
-    for i, col in enumerate(cols):
-        col.selectbox(f'Make a Selection', ['click', 'or click'], key=i)
-    submitted = st.form_submit_button('Submit')
 
 deepdj_api_url = "https://deepdj-7ah34aow4a-ew.a.run.app"
 #deepdj_api_url = "http://127.0.0.1:8000"
